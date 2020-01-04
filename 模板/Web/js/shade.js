daa35f8a13d10cdd0d9b6d49100b4ecdebf8cc8d
bojue <1756085809@qq.com>
2020-01-04 18:21:49 +0800

    项目初始设计
    模板寻找与功能设计数据库设计

---
+++ 模板/Web/js/shade.js
@@ -0,0 +1,33 @@
+// JavaScript Document
+
+function ShowDiv(show_div,bg_div){
+	document.getElementById(show_div).style.display='block';
+	document.getElementById(bg_div).style.display='block' ;
+	var bgdiv = document.getElementById(bg_div);
+	bgdiv.style.width = document.body.scrollWidth;
+	// bgdiv.style.height = $(document).height();
+	jQuery("#"+bg_div).height(jQuery(document).height());
+};
+
+function CloseDiv(show_div,bg_div)
+{
+	document.getElementById(show_div).style.display='none';
+	document.getElementById(bg_div).style.display='none';
+};
+
+
+
+function ShowDiv_1(show_div,bg_div){
+	document.getElementById(show_div).style.display='block';
+	document.getElementById(bg_div).style.display='block' ;
+	var bgdiv = document.getElementById(bg_div);
+	bgdiv.style.width = document.body.scrollWidth;
+	// bgdiv.style.height = $(document).height();
+	jQuery("#"+bg_div).height(jQuery(document).height());
+};
+//¹Ø±Õµ¯³ö²ã
+function CloseDiv_1(show_div,bg_div)
+{
+	document.getElementById(show_div).style.display='none';
+	document.getElementById(bg_div).style.display='none';
+};
